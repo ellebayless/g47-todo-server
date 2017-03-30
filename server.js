5324c80db5abfc9a845b50e4117a91dc5be83aa8
ellebayless <elle@ellebayles.com>
2017-03-30 10:23:20 -0700

    added config and routes

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
-
 var express = require('express');
 var todos = require('./todos.json');
 var bodyParser = require('body-parser');
 
 var app = express();
 
+//Configuration
 app.use(bodyParser.json());
-app.use(express.static('stuff'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(express.static('public'));
 
-console.log(todos);
-
-app.get('/todos', function (req, res) {
-  res.send(todos);
+//Routes
+app.get('/todos', function(req, res) {
+    res.send(todos);
 });
+
 app.get('/todos/:id', function(req, res) {
-var pickedTodo = todo.filter(function(number) {
-console.log(req.param.id);
-return number.id === req.params.id;
-});
-res.send(todos);
+    res.send(todos[req.params.id]);
 });
 
+app.post('/todos', function(req, res) {
+    todos.push(req.body);
+    res.send(todos);
+});
+
+app.put('/todos/:id', function(req, res) {
+    var pickedTodo = todo.filter(function(number) {
+        return number.id === parseInt(req.params.id);
+    });
+    res.send(todos);
+});
+
+app.delete('/todos/:id', function(req, res) {
+    var todoById = req.params.id;
+    todos.splice(todoById, 1);
+    res.send(todos);
+});
+
+//Listener function
 app.listen(8000, function() {
-  console.log('listening on port 8000');
+    console.log('listening on port 8000');
 });
